8a5dba040c108bbf7f5f6b2714eeaf0daa35b749
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-10-27 15:13:59 -0600

    Added a psuedo text file and added variables to the js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = prompt('Do you want to use lowercase?');
+var upperCase = prompt("Do you want to use uppercase>");
+var numerica = prompt('Do you want to use numbers?');
+var specialChars = prompt('Do you want to use special characters?')
 
 // Write password to the #password input
 function writePassword() {
