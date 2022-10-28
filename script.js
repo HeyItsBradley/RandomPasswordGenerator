98de03a7e491e215612160faa4011afe0aa59f4d
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-10-27 19:53:04 -0600

    added the generating for the password and made it display

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
+function generatePassword(){
 
 //These arrays will house the different characters that can be used
 var upperCaseArr =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
 var lowerCaseArr =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 var specialArr =['!', '@', '#', '$', '*', '%'] 
-
 var numberArr =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
+//this var checks is the selected char is within range and if its not return error
+var wantedChar = prompt("How many characters would you like to use? Must be between 8-128 characters")
+if (wantedChar < 8 || wantedChar >= 128){
+  alert('Entered Value must be between 8 and 128!!!')
+  return "Error not within accepted character length";
+}
 
 //These variables will select the users choises
-var wantedChar = prompt("How many characters would you like to use? Must be between 8-128 characters")
 var lowerCase = confirm('Do you want to use lowercase?');
 var upperCase = confirm("Do you want to use uppercase?");
 var numerica = confirm('Do you want to use numbers?');
 var specialChars = confirm('Do you want to use special characters?')
+//This will set selected characts to a blank array
+var selectedChar =[];
 
-var wantedChar=[]
+console.log("This is the ammount of characters that the user wants: " + wantedChar);
 
-//True or false for different passwords types
+// //True or false for different passwords types
 if (upperCase){
-  wantedChar = wantedChar.concat(upperCaseArr);
+  selectedChar = selectedChar.concat(upperCaseArr);  
 }
 if (lowerCase){
-  wantedChar = wantedChar.concat(lowerCaseArr);
+  selectedChar = selectedChar.concat(lowerCaseArr);  
 }
 if (specialChars){
-  wantedChar = wantedChar.concat(specialArr);
-}
+  selectedChar = selectedChar.concat(specialArr);
+} 
 if (numerica){
-  wantedChar = wantedChar.concat(numberArr);
+  selectedChar = selectedChar.concat(numberArr);
 }
+//After user has selected the types of characters programs, all selected characters will now be concatinated into a single array
+//This shows the characts they wanted
+console.log("This will show the whole list of avaiable characters depending on user selection: "+ selectedChar)
+    
 
 //Final password randomizer
+
+//this sets a var finalPass to an empty array
 var finalPass = [];
 
-for (var i=0; i<lengthInput; i++){
-  random
-}
+//    
+for (var i=0; i<=wantedChar; i++){
+   randomNum = Math.floor(Math.random() * selectedChar.length);
+   finalPass = finalPass.concat(selectedChar[randomNum]);
+  }
+  console.log("final password is: " +finalPass); 
+
+  finalPass = finalPass.join('');
+
+  return finalPass;
+}  
+
 
 //Assignment code
 var generateBtn = document.querySelector("#generate");
-password="hello"
-// Write password to the #password input
+      
+ // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); 
   
   passwordText.value = password;
-  
-}
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
